afacc31ae9f9dd8722efc72992198e94ac2b93ae
hogehoge <example@example.com>
2023-02-20 14:47:53 +0900

    first-commit

---
+++ src/common/ExternalSiteCard.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+interface ExternalSiteCardProps {
+    siteName: string;
+    siteUrl: string;
+    siteIcon: string;
+    siteIconDescription?: string;
+    // ?つけるとなくてもエラー吐かない
+}
+
+const ExternalSiteCard = (props: ExternalSiteCardProps) => {
+    return (<div key={props.siteName}>
+        <h3>
+            <a href={props.siteUrl}
+                target="_blank"
+                rel="noopener noreferrer">
+                <Image
+                    src={props.siteIcon}
+                    alt="Next.js Logo"
+                    width={30}
+                    height={15}
+                    priority
+                />
+                {props.siteName}
+            </a>
+        </h3>
+    </div>
+    );
+};
+export default ExternalSiteCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import ExternalSiteCard from "../common/ExternalSiteCard";
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const externalWebsites = [
+    {
+      name: "Github",
+      url: "https://github.com/masamichi0207",
+      logo: "/git.png",
+      imageAlt: "Github logo",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/masamichi7227/",
+      logo: "/insta.png",
+      imageAlt: "insta logo",
+    },
+  ]
   return (
     <>
       <Head>
@@ -15,107 +29,35 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+        <div >
+          <div>高木政道(Takagi,masamichi)</div>
+          <div>2002年生まれ</div>
+          <div>三重県出身</div>
+          <div>長岡技術科学大学　情報・経営システム工学課程　3年</div>
         </div>
-
-        <div className={styles.center}>
+        <div >
           <Image
-            className={styles.logo}
-            src="/next.svg"
+            src="/masa.JPG"
             alt="Next.js Logo"
-            width={180}
-            height={37}
+            width={400}
+            height={200}
             priority
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
         </div>
+        <div>
+          <h2>外部サイト</h2>
+          <div >
+            {externalWebsites.map((Website) => (
+              <ExternalSiteCard
+                key={Website.name}
+                siteName={Website.name}
+                siteUrl={Website.url}
+                siteIcon={Website.logo}
+                siteIconDescription={Website.imageAlt}
+              />
+            ))}
+          </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
